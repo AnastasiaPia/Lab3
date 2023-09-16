7d9c2af90e729fb5c89b2f465791cf21a3cf327f
AnastasiaPia <118008882+AnastasiaPia@users.noreply.github.com>
2023-09-16 20:05:35 +0200

    changes for the documentation

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -1,5 +1,73 @@
+#'Dijkstra Algorithm for the shortest path.
+#'
+#'@param graph It must be data frame. The graph is a data frame including three variables v1,v2,w and contains the edges of the graph from v1 to v2 and the weight of the edge w.
+#'
+#'@param init_node It must be numeric scalar. The initial node that the algorithm will start calculating the shortest path.
+#'
+#'@return It will be a vector with the shortest paths from the initial node.
 
-dijkstra<-function(){
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+dijkstra <- function(graph, init_node) {
+  all_nodes <- unique(c(graph$v1, graph$v2))
+  if (!is.numeric(init_node) || length(init_node) != 1) {
+    stop("the initial node should be a numerical scalar")
+  }
+  if (!(init_node %in% all_nodes)) {
+    stop("the initial node must be included in the list")
+  }
+
+  length_nodes <- length(all_nodes)
+  distance_nodes <- rep(Inf, length_nodes)
+  distance_nodes[init_node] <- 0
+  visited_nodes <- numeric(0)
+
+  while (length(visited_nodes) < length_nodes) {
+    unvisited_nodes <- setdiff(all_nodes, visited_nodes)
+
+    min_distance_nodes <- Inf
+    min_dist <- NULL
+
+    for (node in unvisited_nodes) {
+      if (distance_nodes[node] < min_distance_nodes) {
+        min_distance_nodes <- distance_nodes[node]
+        min_dist <- node
+      }
+    }
+
+    visited_nodes <- c(visited_nodes, min_dist)
+
+    connected_nodes <- unique(c(graph$v1[graph$v2 == min_dist], graph$v2[graph$v1 == min_dist]))
+
+    for (neighbor in connected_nodes) {
+      new_dist <- distance_nodes[min_dist] + graph$w[which(graph$v1 == min_dist & graph$v2 == neighbor)]
+      if (new_dist < distance_nodes[neighbor]) {
+        distance_nodes[neighbor] <- new_dist
+      }
+    }
+  }
+
+  return(distance_nodes)
 }
+
+
--- R/euclidean.R
+++ R/euclidean.R
@@ -1,15 +1,52 @@
 
 
-euclidean<-function(a,b){
- # if(!is.numeric(a)||!is.numeric(b)||!length(a)==1||!length(b)==1){
-  #  stop("check your input")
-  #}
-    while(b!=0){
-      k<-b
-      b<-a%%b
-      a<-k
-    }
-    return(a)
+
+
+#'The function euclidean calculates the greatest common divisor of two numbers
+#'
+#'@param a,b Numeric scalar or integer. The inputs for the euclidean calculation.
+#'@param k Numeric scalar or integer. This variable is used temporarily for storing value of b during the calculation.
+#'
+#'@description
+#'The function takes two integers or numeric scalars. The algorithm has while loop for the calculation.
+#' The loop continues until the value of b is not equal to zero.
+#' Inside the loop value of be is stored in k temporarily.
+#' Then calculating the remainder with mod. This calculation result is stored in b.
+#' Variable a gets the value of k to make the calculation go on.
+#' When the value of b is equal to zero function returns the updated value of a which is the greatest common divisor of the initial two numbers.
+#'
+#'@return If all input are numeric scalars or integers then the output will be scalar or integer. The function will return the greatest common divisor of the two input numbers.
+#'If the input has different type then the function with return error message.
+#'
+#'@examples
+#'#example 1
+#'euclidean(123612, 13892347912)
+#'
+#'#example 2
+#'euclidean(100, 1000)
+#'
+#'\dontrun{
+#'euclidean(TRUE,50)
+#'}
+#'
+#'@seealso
+#'(\url{https://en.wikipedia.org/wiki/Euclidean algorithm}).
+#'
+#'@export
+euclidean <- function(a, b) {
+  if (!is.numeric(a) || !is.numeric(b) || !length(a) == 1 ||
+      !length(b) == 1) {
+    stop("check your input")
+  }
+
+  a <- abs(a)
+  b <- abs(b)
+
+  while (b != 0) {
+    k <- b
+    b <- a %% b
+    a <- k
+  }
+
+  return(a)
 }
-
-
